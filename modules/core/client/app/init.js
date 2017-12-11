50643d51c4bd9515d31484433e68da052bb2cd1b
Mark Wooff <mark.wooff@gmail.com>
2017-12-11 12:03:08 -0500

    Disable caching for IE
    Addresses issue #18

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -20,6 +20,16 @@
 
     $httpProvider.interceptors.push('authInterceptor');
 
+    // Initialize 'get' if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};    
+    }    
+
+    // Disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+
     // Disable debug data for production environment
     // @link https://docs.angularjs.org/guide/production
     $compileProvider.debugInfoEnabled(app.applicationEnvironment !== 'production');
