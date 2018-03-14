3ae6aee3ee4855ee2762a91252cf1b91bf68efa7
Mark Wooff <mark.wooff@gmail.com>
2018-03-14 12:03:42 -0400

    Typo

--- modules/opportunities/client/config/opportunities.client.config.js
+++ modules/opportunities/client/config/opportunities.client.config.js
@@ -4,7 +4,7 @@
 	angular.module('programs').run(['menuService', function (menuService) {
 		menuService.addMenuItem ('topbar', {
 			title: 'Opportunities',
-			state: lang + '.opportunities.list',
+			state: 'opportunities.list',
 			roles: ['*'],
 			icon: 'none',
 			position: 5
